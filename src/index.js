912961dca6d26f73d6f3b6b82987f626c30a7522
annaraga <79105556+annaraga@users.noreply.github.com>
2021-04-11 10:21:58 +0200

    connected weather data with the api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 function showTemperature (response){
 
-console.log(response.data);
+
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#weather-description");
+let degreesElement = document.querySelector("#degrees");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+degreesElement.innerHTML = Math.round(response.data.main.temp);
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+
 
 }
 
